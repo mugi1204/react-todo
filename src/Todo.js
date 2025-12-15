91edd1a7a502b017a6d522a686cb6963970bd337
mugi1204 <210929173+mugi1204@users.noreply.github.com>
2025-12-15 10:56:16 +0900

    インデント修正

--- src/Todo.js
+++ src/Todo.js
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Todo = ({ todo,toggleTodo }) => {
 
-const handleTodoClick = () => {
+  const handleTodoClick = () => {
     toggleTodo(todo.id);
-}
+  }
 
   return (
-      <div>
-        <label>
-            <input type='checkbox' checked={todo.completed} readOnly onChange={handleTodoClick} />
-        </label>
-        {todo.name}
-      </div>
+    <div>
+      <label>
+          <input type='checkbox' checked={todo.completed} readOnly onChange={handleTodoClick} />
+      </label>
+      {todo.name}
+    </div>
   );
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,4 @@
-import React from 'react';
 import Todo from "./Todo";
-import { v4 as uuidv4 } from 'uuid'
 
 const TodoList = ({ todos, toggleTodo }) => {
   return todos.map((todo) => <Todo todo={todo} key={todo.id} toggleTodo={toggleTodo} />)
